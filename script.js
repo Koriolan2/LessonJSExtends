cdb1d6c7fa90c4b36e7ffc9e0ffdc1ff31fe53f0
Kozmin Jurii <Koriolan2@gmail.com>
2020-11-25 23:09:47 +0200

    Lesson4b

--- script.js
+++ script.js
@@ -1,42 +1,38 @@
-'use strict'
-
-const lang = 'ru';
-let rezult ='';
-let rezult2 ='';
-let rezult3 ='';
-if (lang === 'en') {
-    rezult = 'Monday, Tuseday, Wendsday, Thirstday, Friday, Saturday, Sunday';
-} else if (lang === 'ru') {
-    rezult = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье';
-} else {
-    rezult = 'что-то не так :(';
+// Анализ, является ли строкой 
+const isString = function(sts) {
+    if (typeof sts !== 'string') {
+        return true;
+    } else {
+        return false;
+    }
+    
 }
 
-console.log(rezult);
+// Чистка строки и обрезание по длине
+const excerpt = function(st) {
+        st = st.trim();
+        
+        if (st.length > 30) {
+            return st.substr(0, 30) + '...';
+        } else {
+            return st;
+        }
+    }
 
-switch (lang) {
-    case 'en' : rezult2 = 'Monday, Tuseday, Wendsday, Thirstday, Friday, Saturday, Sunday'; break;
-    case 'ru' : rezult2 = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'; break;
-    default: rezult2 = 'что-то не так :('; break;
+// Главный анализатор
+const textAnalitic = function(bigText) {
+
+    let newStr;
+
+    if (isString(bigText)) {
+        newStr = 'Не является строкой';
+    } else {
+        newStr = excerpt(bigText);
+    }
+    return newStr;
 }
 
-console.log(rezult2);
+// Вызов
+let s = 'Обратите внимание на то, что все значения созданные с помощью ключевого слова';
 
-const arr = {
-    en: 'Monday, Tuseday, Wendsday, Thirstday, Friday, Saturday, Sunday',
-    ru: 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'
-}
-
-rezult3 = arr[lang];
-console.log(rezult3);
-
-// Второе задание
-const namePerson = prompt('введите имя', 'Артем');
-
-let rezult4='';
-
-rezult4 = namePerson === 'Артем' ? 'директор':'студент';
-rezult4 = namePerson === 'Максим' ? 'преподаватель':'студент';
-
-console.log(rezult4);
-
+console.log(textAnalitic(s));
