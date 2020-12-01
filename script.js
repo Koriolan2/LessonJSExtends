f64708e0822cbceb220963cf92288bc17fd01791
Kozmin Jurii <Koriolan2@gmail.com>
2020-12-01 23:52:14 +0200

    Home work for Lesson 09b

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 'use strict'
 
-const arr = ['2345', '567', '400', '3353', '13131', '23487', '28906'];
+let data = newDate();
 
-let twoStart = function(a) {
-    let count = a.length;
-   
-    for(let i = 0; i<count; i++) {
 
-       let c =  a[i].slice(0,1);
 
-       if (c==='2' || c==='4') {
-            console.log(a[i]);
-       }
+let detail = {
+  dWeek:data.getDay(),
+  day:data.getDate(),
+  month:data.getMonth(),
+  year:data.getFullYear(),
+  hour:data.getHours(),
+  minute:data.getMinutes(),
+  seconds:data.getSeconds(),
+  formattingDayOfTheWeek: function() {
+    let str = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота', 'Воскресенье'];
+    return str[detail.dWeek-1];
+  },
+  formattingMonth: function () {
+    let str = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
+    return str[detail.month];
+  },
+  leadingZero: function (n) {
+    return n < 10 ? '0' + n : n;
+  },
+  declineWord: function(n, only, dual, plural) {
+    let c = 0;
+    if (n >= 20) {
+      c = n % 10;
+    } else {
+      c = n;
     }
-};
 
-twoStart(arr);
-
-/* Второе задание */
-
-let  allPrime = function() {
-
-    let n = 100;
-
-    nextPrime:
-    for (let i = 2; i <= n; i++) { 
-    
-      for (let j = 2; j < i; j++) { 
-        if (i % j == 0) continue nextPrime; 
-      }
-    
-      console.log(i + ' Делители этого числа 1 и '+ i);
+    if (c == 1) {
+      return only;
+    } else if (c >= 2 && c <= 4) {
+      return dual;
+    } else {
+      return plural;
     }
-};
+  },
+  formatDate: function() {
+    return detail.leadingZero(detail.day)+'.'+detail.leadingZero(detail.month) + '.' + detail.year;
+  },
+  formatTime: function() {
+    return detail.leadingZero(detail.hour)+':'+detail.leadingZero(detail.minute) + ':' + detail.leadingZero(detail.seconds);
+  }
 
-allPrime();
+
+}
+
+console.log('Сегодня ' +  detail.formattingDayOfTheWeek() + ', ' + detail.day + ' ' + detail.formattingMonth() + ' ' + detail.year + ' года, ' + detail.hour + detail.declineWord(detail.hour,' час ', ' часа ', ' часов ' + detail.minute + ' ' + detail.declineWord(detail.minute, ' минута ', ' минуты ', ' минут ') + detail.seconds + detail.declineWord(detail.seconds, '  секунда ', ' секунды ', ' секунд ')));
+
+console.log(detail.formatDate() + ' - ' + detail.formatTime());
